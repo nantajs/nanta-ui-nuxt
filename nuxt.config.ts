e75e1e8311d98272b3a7f444bc56260c5f10e396
jiangguobao <jiangguobao@bixin.cn>
2022-10-09 13:28:51 +0800

    feat: add antdv mini config deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,41 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import IconsResolver from 'unplugin-icons/resolver';
+const isDev = process.env.NODE_ENV === 'development';
 
+export default defineNuxtConfig({
+    modules: ['nuxt-icon'],
+    vite: {
+        ssr: {
+            noExternal: ['ant-design-vue', 'dayjs'],
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                    // https://www.antdv.com/docs/vue/customize-theme/#Ant-Design-Vue-Less-variables
+                    // modifyVars: antdTheme(),
+                },
+            },
+        },
+        plugins: [
+            Components({
+                resolvers: [
+                    IconsResolver({
+                        prefix: 'Icon',
+                    }),
+                    // resolveIcons true will error with NITRO_PRESET=cloudflare
+                    AntDesignVueResolver({ resolveIcons: false, importStyle: 'less' }),
+                ],
+                dts: 'types/components.d.ts',
+            }),
+        ],
+        esbuild: isDev
+            ? {}
+            : {
+                pure: !isDev ? ['console.log', 'console.warn', 'debugger'] : [],
+                legalComments: 'none',
+            },
+    },
 })
