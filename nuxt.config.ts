0a55837472a46ac2201e2aaa1d09fdbbd0ab9203
aborn <loveaborn@foxmail.com>
2022-11-03 20:12:16 +0800

    feat: replace path using vite plugin.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,40 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import path from 'path'
+import filterReplace from './build/filterReplace';
 
 export default defineNuxtConfig({
-    modules: ['nuxt-icon'],
-    vite: {
-        resolve: {
-            alias: {
-              "/@": path.resolve(__dirname, "./src"),
-              "/~": path.resolve(__dirname, "../nanta-ui/src"),
-            }
+  app: {
+    baseURL: process.env.MINDPRESS_MODE === 'ghpages' ? '/nanta-ui-nuxt/' : '/',
+  },
+  modules: ['nuxt-icon'],
+  vite: {
+    plugins: [filterReplace(
+      [
+        {
+          filter: /\.vue$/,
+          replace: {
+            from: /\/~\/main/g,
+            to: '@nanta/ui'
           },
-    }
+        },
+        {
+          filter: /\.ts$/,
+          replace: {
+            from: /\/~\/main/g,
+            to: '@nanta/ui'
+          },
+        },
+      ],
+      {
+        enforce: 'pre',
+        apply: 'build'
+      }
+    )],
+    resolve: {
+      alias: {
+        "/@": path.resolve(__dirname, "./src"),
+        "/~": path.resolve(__dirname, "../nanta-ui/src"),
+      }
+    },
+  }
 })
